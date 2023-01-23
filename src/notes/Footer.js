1bfa9c9cf954459d8e9487583c8854a855eda4bf
Bishwa Kiran Poudel <bishwakiran725@gmail.com>
2023-01-23 22:35:34 +0545

    Initial outlining

---
+++ src/notes/Footer.js
@@ -0,0 +1,11 @@
+import { Button } from '@mui/material';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <></>
+    )
+}
+
+export default Footer
